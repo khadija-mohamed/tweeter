6d0eb277e1886d48f014a2b8dcedeb9af44c6eb7
Khadija Mohamed <khadijam@live.ca>
2022-02-07 08:38:28 -0500

    counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".tweet-text").on('input', function() {
+    let charactersCounted = $(this).val().length;
+    let remainingCharacters = 140 - charactersCounted;
+
+    let counter = $(this).parent().next('div').children('.counter');
+    counter.text(remainingCharacters);
+  });
+});
